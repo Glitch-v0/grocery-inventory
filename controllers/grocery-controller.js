90130a80abe2804806af44fac96a933072f8721a
Nathan Jones <njonessurvey@gmail.com>
2024-08-30 16:39:31 -0500

    Add categories view

--- controllers/grocery-controller.js
+++ controllers/grocery-controller.js
@@ -9,13 +9,26 @@ async function getGroceryList (req, res) {
 }
 
 async function getGroceryItem (req, res) {
-    const itemID = req.params.id;
+    const itemID = parseInt(req.params.id);
+    console.log("Received ID:", req.params.id);
+    if (!itemID) {
+        return res.render("error", {errorCode: 404, errorMessage: "Invalid Page"});
+    }
     const groceryItem = await db.getItem(itemID); // returns 4 rows
-    console.log(groceryItem)
+    if (groceryItem.length === 0) {
+        // Handle the case where no rows are found
+        res.render("error", {errorCode: 404, errorMessage: "Item not found"});
+      }
     res.render("grocery-item", {itemStats: groceryItem});
+}
+
+async function getCategory (req, res) {
+    const categoryList = await db.getCategory();
+    res.render("category", {itemCategories: categoryList});
 }
 
 module.exports = {
     getGroceryList,
-    getGroceryItem
+    getGroceryItem,
+    getCategory
 }
--- routes/grocery-routes.js
+++ routes/grocery-routes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const groceryController = require("../controllers/grocery-controller");
 
 router.get("/", groceryController.getGroceryList);
-router.get("/:id", groceryController.getGroceryItem);
+router.get("/item/:id", groceryController.getGroceryItem);
+router.get("/category", groceryController.getCategory)
+router.get("/*", (req, res) => {
+    res.render("error", {errorCode: 404, errorMessage: "Invalid Page"});
+});
 // router.post("/", groceryController.addGroceryItem);
 // router.post("/:id", groceryController.updateGroceryItem);
 // router.delete("/:id", groceryController.deleteGroceryItem);
