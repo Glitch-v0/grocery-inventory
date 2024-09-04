7d91ad6f8c3cf8699d4a92977c32bb667ab5a3ed
Nathan Jones <njonessurvey@gmail.com>
2024-09-03 21:18:55 -0500

    item page is new home page, fix label pointers for add forms.

--- routes/grocery-routes.js
+++ routes/grocery-routes.js
@@ -4,19 +4,26 @@ const router = express.Router();
 const groceryController = require('../controllers/grocery-controller');
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.redirect('/items');
 });
 router.get('/items', groceryController.getGroceryList);
+// router.post("/items", groceryController.addItem);
 router.get('/items/:id', groceryController.getGroceryItem);
-// router.post("/:id", groceryController.updateGroceryItem);
+//router.put('/items/:id', groceryController.updateItem);
+//router.delete(/items/:id", groceryController.deleteGroceryItem);
+
 router.get('/categories', groceryController.getCategories);
 router.post('/categories', groceryController.addNewCategory);
+//router.put('/categories/:id', groceryController.updateCategory);
+//router.delete('/categories/:id', groceryController.deleteCategory);
+
 router.get('/warehouses', groceryController.getWarehouses);
+//router.post('/warehouses', groceryController.addNewWarehouse);
+//router.put('/warehouses/:id', groceryController.updateWarehouse);
+//router.delete('/warehouses/:id', groceryController.deleteWarehouse);
+
 router.get('/*', (req, res) => {
   res.render('error', { errorCode: 404, errorMessage: 'Invalid Page' });
 });
-// router.post("/", groceryController.addGroceryItem);
-
-// router.delete("/:id", groceryController.deleteGroceryItem);
 
 module.exports = router;
